78ad5ed6ba8c18a21dea0056318818a0e92e9abe
austin-thwoo <austin-thwoo@naver.com>
2023-01-26 11:48:29 +0900

    삼각형의 완성조건(1)-java 프로그래머스-2023.01.26 Array.sort

---
+++ src/programers/Jan26_2023.java
@@ -0,0 +1,25 @@
+package programers;
+
+import java.util.Arrays;
+
+public class Jan26_2023 {
+
+    class Solution {
+        public int solution(int[] sides) {
+
+            int answer = 0;
+            Arrays.sort(sides);
+
+            if( sides[2] < (sides[0] + sides[1]) ) {
+                answer = 1;
+            } else {
+                answer = 2;
+            }
+
+
+            return answer;
+        }
+    }
+
+
+}
