0c2f4c78cb50165969898422420572cbac3eca4e
ausitn-thwoo <austin-thwoo@naver.com>
2023-02-06 22:35:21 +0900

    stack(1)-java 리크코드20-2023.02.06 stack

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,42 @@
 import programers.Jan26_2023;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
 
+        class Solution {
+            public boolean isValid(String s) {
 
+                int len = s.length();
+
+                if(len % 2 != 0) {return false;}
+
+                Stack<Character> stack = new Stack<>();
+
+                for(int i = 0; i < len; i++) {
+                    char curn = s.charAt(i);
+
+                    if(curn == '(' || curn == '{' || curn == '[') {
+                        stack.push(curn);
+                    }
+
+                    else if(stack.size() == 0) {return false;}
+
+                    else if(curn == ')') {
+                        if(stack.pop() != '(') {return false;}
+                    }
+                    else if(curn == '}') {
+                        if(stack.pop() != '{') {return false;}
+                    }
+                    else if(curn == ']') {
+                        if(stack.pop() != '[') {return false;}
+                    }
+                }
+                return stack.size() == 0;
+            }
+        }
 
 
 
