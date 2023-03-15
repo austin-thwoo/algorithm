08f10bdf9cf02a57f1e0f9b022625fdf961631a9
austin-thwoo <austin-thwoo@naver.com>
2023-03-15 10:58:12 +0900

    leetcode-122 greedy algorithm-java 2023.03.15 metrix version : 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,35 @@
 import programers.Jan26_2023;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
 import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Practice practice = new Practice();
-        int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        int[] price ={7,1,5,3,6,4};
+        int a =practice.maxProfit(price);
+        System.out.println(a);
 
-        System.out.println(practice.spiralOrder(matrix).stream().collect(Collectors.toList()));
+
+
 
     }
+
+
+
+//    int[] res = new int[2];
+//    Hashtable<Integer,Integer> ht = new Hashtable<>();
+//    for(int i = 0; i < nums.length; i++){
+//        if(ht.containsKey(target-nums[i])){
+//            res[0] = i;
+//            res[1] = ht.get(target-nums[i]);
+//            return res;
+//        }
+//        ht.put(nums[i],i);
+//    }
+//        return res;
+//}
 }
--- src/Practice.java
+++ src/Practice.java
@@ -4,85 +4,23 @@ import java.util.stream.Collectors;
 public class Practice {
 
 
-    public List<Integer> spiralOrder(int[][] matrix) {
 
-//         int[] result = new int[matrix.length+matrix[0].length];
-        List<Integer> result = new ArrayList();
-        int size=matrix.length+matrix[0].length;
+    public int maxProfit(int[] prices) {
+        int cost = 0;
 
-        int left = 0;
-        int right = matrix[0].length - 1;
-        int top = 0;
-        int bottom = matrix.length - 1;
-        int count = 0;
-//         public void addResult (int r){
-//          result[count]=r;
-//          count++;
-//        }
-        while (top<=bottom && left<=right) {
-
-            for (int i = left; i <= right; i++) {
-                result.add(matrix[top][i]);
-                System.out.println("1for" + count);
-                count++;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                cost += prices[i] - prices[i-1];
             }
-
-            top++;
-
-
-//l r
-//xxx
-//000  t
-//000  b
-            for (int j = top; j <= bottom; j++) {
-                result.add(matrix[j][right]);
-                System.out.println("2for" + count);
-                count++;
-            }
-
-            right--;
-
-
-//lr
-//xxx
-//00x  t
-//00x  b
-            if (top<=bottom) {
-                for (int k = right; k >= left; k--) {
-                    result.add(matrix[bottom][k]);
-                    System.out.println("3for" + count);
-                    count++;
-
-                }
-                System.out.println("??" + result.stream().collect(Collectors.toList()));
-                bottom--;
-
-
-            }
-//lr
-//xxx
-//00x t b
-//xxx
-            if (left<=right) {
-                for (int l = bottom; l >= top; l--) {
-                    result.add(matrix[l][left]);
-                    System.out.println("4for" + count);
-                    System.out.println("??" + result.stream().collect(Collectors.toList()));
-                    count++;
-                }
-                left++;
-
-
-            }
-//lr
-//xxx
-//x0x t b
-//xxx
         }
-
-
-
-        return result;
+        return cost;
     }
 
+
+
+
 }
+
+
+
+
