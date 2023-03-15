a048efcce9fc829dd989d6fdce46f0aac1b499f2
austin-thwoo <austin-thwoo@naver.com>
2023-03-15 11:00:19 +0900

    leetcode-122 greedy algorithm-java 2023.03.15 metrix version : 1

---
+++ src/litecode/greedy/Mar15_3_2023_1.java
@@ -0,0 +1,17 @@
+package litecode.greedy;
+
+import java.util.Stack;
+
+public class Mar15_3_2023_1 {
+
+    public int maxProfit(int[] prices) {
+        int cost = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                cost += prices[i] - prices[i - 1];
+            }
+        }
+        return cost;
+    }
+}
